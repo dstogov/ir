fc2266d61ab1e9c48a5aadb419b2307f3ee8b7d3
Dmitry Stogov <dmitry@zend.com>
2022-06-21 15:09:26 +0300

    Remove PHP specific registers

--- ir_php.h
+++ ir_php.h
@@ -3,20 +3,4 @@
 
 #define IR_PHP_OPS(_)
 
-#if defined(IR_TARGET_X86)
-# define IR_REG_PHP_FP IR_REG_RSI
-# define IR_REG_PHP_IP IR_REG_RDI
-#elif defined(IR_TARGET_X64)
-# define IR_REG_PHP_FP IR_REG_R14
-# define IR_REG_PHP_IP IR_REG_R15
-#elif defined(IR_TARGET_AARCH64)
-# define IR_REG_PHP_FP IR_REG_X27
-# define IR_REG_PHP_IP IR_REG_X28
-#else
-# error "Unknown IR target"
-#endif
-
-#define IR_REGSET_PHP_FIXED \
-	(IR_REGSET(IR_REG_PHP_FP) | IR_REGSET(IR_REG_PHP_IP))
-
 #endif /* IR_PHP_H */
