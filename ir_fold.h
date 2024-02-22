10f49102f336ff15808e582fddd4251f27ad0f5c
Dmitry Stogov <dmitry@zend.com>
2024-02-22 09:32:28 +0300

    Add boolean simplification folding rules

--- ir_fold.h
+++ ir_fold.h
@@ -1414,6 +1414,73 @@ IR_FOLD(BSWAP(BSWAP))
 	IR_FOLD_COPY(op1_insn->op1);
 }
 
+IR_FOLD(EQ(_, C_BOOL))
+{
+	if (op2 == IR_TRUE) {
+		IR_FOLD_COPY(op1);
+	} else {
+		opt = IR_OPT(IR_NOT, IR_BOOL);
+		op2 = IR_UNUSED;
+		IR_FOLD_RESTART;
+	}
+}
+
+IR_FOLD(NE(_, C_BOOL))
+{
+	if (op2 != IR_TRUE) {
+		IR_FOLD_COPY(op1);
+	} else {
+		opt = IR_OPT(IR_NOT, IR_BOOL);
+		op2 = IR_UNUSED;
+		IR_FOLD_RESTART;
+	}
+}
+
+IR_FOLD(EQ(ZEXT, C_U16))
+IR_FOLD(EQ(ZEXT, C_U32))
+IR_FOLD(EQ(ZEXT, C_U64))
+IR_FOLD(EQ(ZEXT, C_I16))
+IR_FOLD(EQ(ZEXT, C_I32))
+IR_FOLD(EQ(ZEXT, C_I64))
+IR_FOLD(EQ(ZEXT, C_ADDR))
+IR_FOLD(EQ(SEXT, C_U16))
+IR_FOLD(EQ(SEXT, C_U32))
+IR_FOLD(EQ(SEXT, C_U64))
+IR_FOLD(EQ(SEXT, C_I16))
+IR_FOLD(EQ(SEXT, C_I32))
+IR_FOLD(EQ(SEXT, C_I64))
+IR_FOLD(EQ(SEXT, C_ADDR))
+{
+	if (op2_insn->val.u64 == 0 && ctx->ir_base[op1_insn->op1].type == IR_BOOL) {
+		opt = IR_OPT(IR_NOT, IR_BOOL);
+		op1 = op1_insn->op1;
+		op2 = IR_UNUSED;
+		IR_FOLD_RESTART;
+	}
+	IR_FOLD_NEXT;
+}
+
+IR_FOLD(NE(ZEXT, C_U16))
+IR_FOLD(NE(ZEXT, C_U32))
+IR_FOLD(NE(ZEXT, C_U64))
+IR_FOLD(NE(ZEXT, C_I16))
+IR_FOLD(NE(ZEXT, C_I32))
+IR_FOLD(NE(ZEXT, C_I64))
+IR_FOLD(NE(ZEXT, C_ADDR))
+IR_FOLD(NE(SEXT, C_U16))
+IR_FOLD(NE(SEXT, C_U32))
+IR_FOLD(NE(SEXT, C_U64))
+IR_FOLD(NE(SEXT, C_I16))
+IR_FOLD(NE(SEXT, C_I32))
+IR_FOLD(NE(SEXT, C_I64))
+IR_FOLD(NE(SEXT, C_ADDR))
+{
+	if (op2_insn->val.u64 == 0 && ctx->ir_base[op1_insn->op1].type == IR_BOOL) {
+		IR_FOLD_COPY(op1_insn->op1);
+	}
+	IR_FOLD_NEXT;
+}
+
 IR_FOLD(NOT(EQ))
 IR_FOLD(NOT(NE))
 IR_FOLD(NOT(LT))
