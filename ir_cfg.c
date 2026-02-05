57fb9b89cd3fb6e7c811b41fafbfc847e9f92e76
Dmitry Stogov <dmitrystogov@gmail.com>
2026-02-05 18:41:48 +0300

    Fix GH-125: Handle phis in ir_cfg_remove_dead_inputs() (#127)

--- ir_cfg.c
+++ ir_cfg.c
@@ -77,12 +77,15 @@ void ir_reset_cfg(ir_ctx *ctx)
 	}
 }
 
+static void ir_remove_phis_inputs(ir_ctx *ctx, ir_use_list *use_list, int new_inputs_count, ir_bitset life_inputs);
+
 static uint32_t IR_NEVER_INLINE ir_cfg_remove_dead_inputs(ir_ctx *ctx, uint32_t *_blocks, ir_block *blocks, uint32_t bb_count)
 {
 	uint32_t b, count = 0;
 	ir_block *bb = blocks + 1;
 	ir_insn *insn;
 	ir_ref i, j, n, *ops, input;
+	ir_bitset life_inputs = NULL;
 
 	for (b = 1; b <= bb_count; b++, bb++) {
 		bb->successors = count;
@@ -96,12 +99,27 @@ static uint32_t IR_NEVER_INLINE ir_cfg_remove_dead_inputs(ir_ctx *ctx, uint32_t 
 			for (i = 1, j = 1; i <= n; i++) {
 				input = ops[i];
 				if (_blocks[input]) {
+					if (life_inputs) {
+						ir_bitset_incl(life_inputs, i);
+					}
 					if (i != j) {
 						ops[j] = ops[i];
 					}
 					j++;
-				} else if (input > 0) {
-					ir_use_list_remove_one(ctx, input, bb->start);
+				} else {
+					if (ctx->use_lists[bb->start].count > 1) {
+						/* Some inputs of this MERGE are deleted and we have to update the depended PHIs */
+						if (!life_inputs) {
+							int k;
+							life_inputs = ir_bitset_malloc(n + 1);
+							for (k = 1; k < i; k++) {
+								ir_bitset_incl(life_inputs, k);
+							}
+						}
+					}
+					if (input > 0) {
+						ir_use_list_remove_one(ctx, input, bb->start);
+					}
 				}
 			}
 			j--;
@@ -114,6 +132,10 @@ static uint32_t IR_NEVER_INLINE ir_cfg_remove_dead_inputs(ir_ctx *ctx, uint32_t 
 				j++;
 				for (;j <= n; j++) {
 					ops[j] = IR_UNUSED;
+				}
+				if (life_inputs) {
+					ir_remove_phis_inputs(ctx, &ctx->use_lists[bb->start], insn->inputs_count, life_inputs);
+					ir_mem_free(life_inputs);
 				}
 			}
 		}
@@ -375,8 +397,7 @@ static void ir_remove_predecessor(ir_ctx *ctx, ir_block *bb, uint32_t from)
 
 static void ir_remove_merge_input(ir_ctx *ctx, ir_ref merge, ir_ref from)
 {
-	ir_ref i, j, n, k, *p, *q, use;
-	ir_insn *use_insn;
+	ir_ref i, j, n;
 	ir_use_list *use_list;
 	ir_bitset life_inputs;
 	ir_insn *insn = &ctx->ir_base[merge];
@@ -402,15 +423,30 @@ static void ir_remove_merge_input(ir_ctx *ctx, ir_ref merge, ir_ref from)
 	}
 	if (i == 1) {
 		insn->op = IR_BEGIN;
-		insn->inputs_count = 1;
-		use_list = &ctx->use_lists[merge];
-		if (use_list->count > 1) {
-			n++;
+	}
+	insn->inputs_count = i;
+
+	use_list = &ctx->use_lists[merge];
+	if (use_list->count > 1) {
+		ir_remove_phis_inputs(ctx, use_list, i, life_inputs);
+	}
+
+	ir_mem_free(life_inputs);
+	ir_use_list_remove_all(ctx, from, merge);
+}
+
+static void ir_remove_phis_inputs(ir_ctx *ctx, ir_use_list *use_list, int new_inputs_count, ir_bitset life_inputs)
+{
+	ir_ref i, j, n, k, *p, *q, use;
+	ir_insn *use_insn;
+
+	if (new_inputs_count == 1) {
 			for (k = use_list->count, p = q = &ctx->use_edges[use_list->refs]; k > 0; p++, k--) {
 				use = *p;
 				use_insn = &ctx->ir_base[use];
 				if (use_insn->op == IR_PHI) {
 					/* Convert PHI to COPY */
+					n = use_insn->inputs_count;
 					i = 2;
 					for (j = 2; j <= n; j++) {
 						ir_ref input = ir_insn_op(use_insn, j);
@@ -443,17 +479,12 @@ static void ir_remove_merge_input(ir_ctx *ctx, ir_ref merge, ir_ref from)
 					q++;
 				} while (p != q);
 			}
-		}
 	} else {
-		insn->inputs_count = i;
-
-		use_list = &ctx->use_lists[merge];
-		if (use_list->count > 1) {
-			n++;
 			for (k = use_list->count, p = &ctx->use_edges[use_list->refs]; k > 0; p++, k--) {
 				use = *p;
 				use_insn = &ctx->ir_base[use];
 				if (use_insn->op == IR_PHI) {
+					n = use_insn->inputs_count;
 					i = 2;
 					for (j = 2; j <= n; j++) {
 						ir_ref input = ir_insn_op(use_insn, j);
@@ -474,10 +505,7 @@ static void ir_remove_merge_input(ir_ctx *ctx, ir_ref merge, ir_ref from)
 					}
 				}
 			}
-		}
 	}
-	ir_mem_free(life_inputs);
-	ir_use_list_remove_all(ctx, from, merge);
 }
 
 /* CFG constructed after SCCP pass doesn't have unreachable BBs, otherwise they should be removed */
