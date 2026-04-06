5a712b8a9d2e105d0c9731f662cfc0c877585ff3
Anatol Belski <anbelski@linux.microsoft.com>
2026-04-06 21:06:39 +0200

    ir_load: Regenerate from ir.g
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- ir_load.c
+++ ir_load.c
@@ -34,6 +34,7 @@ uint32_t yy_line;
 typedef struct _ir_parser_ctx {
 	ir_ctx    *ctx;
 	uint32_t   undef_count;
+	uint32_t   undef_mem_op;
 	uint32_t   value_params_count;
 	ir_ref     curr_ref;
 	ir_strtab  var_tab;
@@ -1504,6 +1505,7 @@ static int parse_ir_sym_data(int sym, ir_loader *loader) {
 
 static int parse_ir_func(int sym, ir_parser_ctx *p) {
 	p->undef_count = 0;
+	p->undef_mem_op = 0;
 	ir_strtab_init(&p->var_tab, 256, 4096);
 	if (sym != YY__LBRACE) {
 		yy_error_sym("'{' expected, got", sym);
@@ -1856,7 +1858,8 @@ static int parse_ir_insn(int sym, ir_parser_ctx *p) {
 					p->ctx->control = IR_UNUSED;
 				} else if (op == IR_VLOAD
 				 && !IR_IS_UNRESOLVED(op1)
-				 && !IR_IS_UNRESOLVED(op2)) {
+				 && !IR_IS_UNRESOLVED(op2)
+				 && !p->undef_mem_op) {
 					p->ctx->control = op1;
 					ref = _ir_VLOAD(p->ctx, t, op2);
 					ref2 = p->ctx->control;
@@ -1864,14 +1867,16 @@ static int parse_ir_insn(int sym, ir_parser_ctx *p) {
 				} else if (op == IR_VSTORE
 				 && !IR_IS_UNRESOLVED(op1)
 				 && !IR_IS_UNRESOLVED(op2)
-				 && !IR_IS_UNRESOLVED(op3)) {
+				 && !IR_IS_UNRESOLVED(op3)
+				 && !p->undef_mem_op) {
 					p->ctx->control = op1;
 					_ir_VSTORE(p->ctx, op2, op3);
 					ref = p->ctx->control;
 					p->ctx->control = IR_UNUSED;
 				} else if (op == IR_LOAD
 				 && !IR_IS_UNRESOLVED(op1)
-				 && !IR_IS_UNRESOLVED(op2)) {
+				 && !IR_IS_UNRESOLVED(op2)
+				 && !p->undef_mem_op) {
 					p->ctx->control = op1;
 					ref = _ir_LOAD(p->ctx, t, op2);
 					ref2 = p->ctx->control;
@@ -1879,7 +1884,8 @@ static int parse_ir_insn(int sym, ir_parser_ctx *p) {
 				} else if (op == IR_STORE
 				 && !IR_IS_UNRESOLVED(op1)
 				 && !IR_IS_UNRESOLVED(op2)
-				 && !IR_IS_UNRESOLVED(op3)) {
+				 && !IR_IS_UNRESOLVED(op3)
+				 && !p->undef_mem_op) {
 					p->ctx->control = op1;
 					_ir_STORE(p->ctx, op2, op3);
 					ref = p->ctx->control;
@@ -1887,6 +1893,10 @@ static int parse_ir_insn(int sym, ir_parser_ctx *p) {
 				} else {
 					uint32_t opt;
 
+					if ((op == IR_VSTORE || op == IR_STORE || op == IR_VLOAD || op == IR_LOAD)
+					 && (IR_IS_UNRESOLVED(op1) || IR_IS_UNRESOLVED(op2) || IR_IS_UNRESOLVED(op3))) {
+						p->undef_mem_op = 1;
+					}
 					if (!IR_OP_HAS_VAR_INPUTS(ir_op_flags[op])) {
 						opt = IR_OPT(op, t);
 					} else {
