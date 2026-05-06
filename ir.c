7fadf60f7e180dd970bed1cb666b705ba291be3d
Dmitry Stogov <dmitry@php.net>
2026-05-06 22:55:19 +0300

    Fix incorrect comparison

--- ir.c
+++ ir.c
@@ -161,7 +161,7 @@ void ir_print_const(const ir_ctx *ctx, const ir_insn *insn, FILE *f, bool quoted
 		case IR_CHAR:
 			if (insn->val.c == '\\') {
 				fprintf(f, "'\\\\'");
-			} else if (insn->val.c >= '\'') {
+			} else if (insn->val.c == '\'') {
 				fprintf(f, "'\\\''");
 			} else if (insn->val.c >= ' ') {
 				fprintf(f, "'%c'", insn->val.c);
