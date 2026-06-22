bf33bf0ff249d1928be5e8277f9472d6b6d5def6
Dmitry Stogov <dmitry@php.net>
2026-06-23 01:46:37 +0300

    Optimize "IF(SHR(SHL(X, C1), C2))" into "IF(AND(X, ((-1) << C2) >> C1)"

--- ir_sccp.c
+++ ir_sccp.c
@@ -3545,7 +3545,47 @@ static ir_ref ir_iter_optimize_condition(ir_ctx *ctx, ir_ref control, ir_ref con
 		if (!IR_IS_SYM_CONST(val_insn->op) && val_insn->val.u64 == 1) {
 			return IR_TRUE;
 		}
-	} else if (condition_insn->op == IR_AND && IR_IS_CONST_REF(condition_insn->op2)) {
+	} else if (condition_insn->op == IR_SHR && IR_IS_CONST_REF(condition_insn->op2)) {
+		ir_insn *val_insn = &ctx->ir_base[condition_insn->op2];
+		ir_insn *op1_insn = &ctx->ir_base[condition_insn->op1];
+
+		if (!IR_IS_SYM_CONST(val_insn->op)
+		 && ctx->use_lists[condition].count == 1
+		 && ctx->use_lists[condition_insn->op1].count == 1
+		 && op1_insn->op == IR_SHL
+		 && IR_IS_CONST_REF(op1_insn->op2)
+		 && !IR_IS_SYM_CONST(ctx->ir_base[op1_insn->op2].op)) {
+			/* IF(SHR(SHL(X, C1), C2)) => IF(AND(X, ((-1) << C2) >> C1) */
+			ir_insn *val2_insn = &ctx->ir_base[op1_insn->op2];
+			ir_val val;
+
+			if (op1_insn->op1 > 0) {
+				ir_use_list_replace_one(ctx, op1_insn->op1, condition_insn->op1, condition);
+			}
+			CLEAR_USES(condition_insn->op1);
+			switch (ir_type_size[condition_insn->type]) {
+				case 1:
+					val.u64 = (uint8_t)((uint8_t)((uint8_t)(-1) << (val_insn->val.u8 & 7)) >> (val2_insn->val.u8 & 7));
+					break;
+				case 2:
+					val.u64 = (uint16_t)((uint16_t)((uint16_t)(-1) << (val_insn->val.u16 & 0xf)) >> (val2_insn->val.u16 & 0xf));
+					break;
+				case 4:
+					val.u64 = (uint32_t)((uint32_t)((uint32_t)(-1) << (val_insn->val.u32 & 0x1f)) >> (val2_insn->val.u32 & 0x1f));
+					break;
+				case 8:
+					val.u64 = (((uint64_t)(-1) << (val_insn->val.u64 & 0x3f)) >> (val2_insn->val.u64 & 0x3f));
+					break;
+				default:
+					IR_ASSERT(0);
+			}
+			condition_insn->op = IR_AND;
+			condition_insn->op1 = op1_insn->op1;
+			condition_insn->op2 = ir_const(ctx, val, condition_insn->type);
+			MAKE_NOP(op1_insn);
+		}
+	}
+	if (condition_insn->op == IR_AND && IR_IS_CONST_REF(condition_insn->op2)) {
 		ir_insn *val_insn = &ctx->ir_base[condition_insn->op2];
 		ir_insn *op1_insn = &ctx->ir_base[condition_insn->op1];
 
@@ -3555,7 +3595,9 @@ static ir_ref ir_iter_optimize_condition(ir_ctx *ctx, ir_ref control, ir_ref con
 		 && (op1_insn->op == IR_ZEXT || op1_insn->op == IR_SEXT)
 		 && val_insn->val.u64 <= (((uint64_t)-1ULL) >> (64 - ir_type_size[ctx->ir_base[op1_insn->op1].type] * 8))) {
 			/* IF(AND(ZEXT(X), C)) => IF(AND(X, C)) */
-			ir_use_list_replace_one(ctx, op1_insn->op1, condition_insn->op1, condition);
+			if (op1_insn->op1 > 0) {
+				ir_use_list_replace_one(ctx, op1_insn->op1, condition_insn->op1, condition);
+			}
 			CLEAR_USES(condition_insn->op1);
 			condition_insn->type = ctx->ir_base[op1_insn->op1].type;
 			condition_insn->op1 = op1_insn->op1;
