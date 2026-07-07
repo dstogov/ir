f7a07fdea0bd2a853ebc876854db1dcf63435545
Dmitry Stogov <dmitry@php.net>
2026-07-07 16:30:31 +0300

    Introduce SHUFFLE instruction

--- ir_builder.h
+++ ir_builder.h
@@ -492,6 +492,7 @@ extern "C" {
 #define ir_EXTRACT(_type, _op1, _op2)     ir_fold2(_ir_CTX, IR_OPT(IR_EXTRACT, (_type)), (_op1), (_op2))
 #define ir_REPLACE(_type, _op1, _op2, _v) ir_fold3(_ir_CTX, IR_OPT(IR_REPLACE, (_type)), (_op1), (_op2), (_v))
 #define ir_SPLAT(_type, _op1)             ir_UNARY_OP(IR_SPLAT, (_type), (_op1))
+#define ir_SHUFFLE(_type, _op1, _op2, _m) ir_fold3(_ir_CTX, IR_OPT(IR_SHUFFLE, (_type)), (_op1), (_op2), (_m))
 
 /* Unfoldable variant of COPY */
 #define ir_HARD_COPY(_type, _op1)         ir_emit2(_ir_CTX, IR_OPT(IR_COPY, (_type)), (_op1), IR_COPY_HARD)
